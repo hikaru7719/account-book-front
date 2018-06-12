9c846a763688ab5bc18d073225411fc35c02b13c
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2018-06-13 00:10:38 +0900

    add datepicker

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,10 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import {DatePicker} from 'material-ui-pickers';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
+import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
+
 
 class Input extends React.Component {
 
@@ -19,6 +23,10 @@ class Input extends React.Component {
                         margin="normal"
                         style={{marginLeft:30,marginRight:30,width:500}}
                         onChange={(e)=>this.props.onChange(e.target.id,e.target.value)}
+                        placeholder="goods name"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
                     />
                     </Grid>
 
@@ -30,6 +38,10 @@ class Input extends React.Component {
                         margin="normal"
                         style={{marginLeft:30,marginRight:30,width:500}}
                         onChange={(e)=>this.props.onChange(e.target.id,e.target.value)}
+                        placeholder="¥999,999"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
                         />
                     </Grid>
                     <Grid item xs={12}>
@@ -39,16 +51,26 @@ class Input extends React.Component {
                         margin="normal"
                         style={{marginLeft:30,marginRight:30,width:500}}
                         onChange={(e)=>this.props.onChange(e.target.id,e.target.value)}
+                        placeholder="衣類"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
                         />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField
-                        id="date"
-                        label="date"
-                        margin="normal"
-                        style={{marginLeft:30,marginRight:30,width:500}}
-                        onChange={(e)=>this.props.onChange(e.target.id,e.target.value)}
-                        />
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <div className="picker">
+                            <DatePicker
+                                label="date"
+                                style={{marginLeft:30,marginRight:30,width:500}}
+                                format="YYYY/MM/DD"
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                        </div>
+                        </MuiPickersUtilsProvider>
                     </Grid>
                     <Grid item xs={12}>
                         <TextField
@@ -57,6 +79,10 @@ class Input extends React.Component {
                         margin="memo"
                         style={{marginLeft:30,marginRight:30,width:500}}
                         onChange={(e)=>this.props.onChange(e.target.id,e.target.value)}
+                        placeholder="free"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
                         />
                     </Grid>
                     <Grid xs={{offset:8}}/>
